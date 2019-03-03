9df27e776eae7a46c38ae7def0c45a8548f191f8
gutiguy <gutiguy.dev@gmail.com>
2019-03-03 07:17:30 +0200

    Added some position styling to withLoading's spinner.

--- client/src/hoc/withLoading.js
+++ client/src/hoc/withLoading.js
@@ -6,14 +6,24 @@ export default function withLoading(LoadingComponent) {
     const { isLoading, ...rest } = props;
     return (
       <React.Fragment>
-        {isLoading ? <RingLoader /> : null}
+        {isLoading ? (
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center"
+            }}
+          >
+            <RingLoader />
+          </div>
+        ) : null}
         <LoadingComponent {...rest} />
       </React.Fragment>
     );
   };
 }
 
-/* Use this when you want the component only to mount when loading is done (and unmount when it resumes) */
+/* Use this when you want the component to mount only when loading is done (and unmount when it resumes) */
 export function withLoadingAndMount(LoadingComponent) {
   return function WithLoadingAndMount(props) {
     const { isLoading, ...rest } = props;
