dd638a39209cb6d318262a075f28d8f0ab8fa438
gutiguy <gutiguy.dev@gmail.com>
2019-03-11 18:50:20 +0200

    Minor changes, exposed config file since the information there is meant to be publicly exposed anyway.

---
+++ client/src/config.js
@@ -0,0 +1,22 @@
+export default Object.freeze({
+  siteTitle: "Glogfolio",
+  email: "example@contact.com",
+
+  socialMediaLinks: {
+    twitter: "https://twitter.com/example",
+    facebook: "https://facebook.com/example",
+    youtube: "https://youtube.com/example",
+    instagram: "https://instagram.com/example"
+  },
+
+  homepageParagraphs: [
+    {
+      title: "About Me",
+      content: "Example."
+    },
+    {
+      title: "General Inquiry",
+      content: "Another Example."
+    }
+  ]
+});
